53f952f6472e9928d888c99fce361546409e60e4
UtaBaramidze <geo.pumpkin.2003@gmail.com>
2024-02-16 01:44:14 +0400

    First Day Of Learning C++

---
+++ FirstStepsInC++/main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+
+
+// typedef std::vector<std::pair<std::string, int>> pairlist_t;
+// typedef std::string text_t;
+// typedef int number_t;
+// using text_t = std::string;
+// using number_t = int;
+
+// namespace first{
+//     int x = 100;
+// }
+// namespace second{
+//     int x = 200;
+// }
+int main(){
+
+    // integer (whole number)
+    // int age = 21;
+    // int year = 2024;
+    
+    // double (number including decimal)
+    // double price = 10.45;
+    // double temperature = 36.6;
+
+    // single character
+    // char grade = 'A';
+    // char initial = 'B';
+
+    // boolean (true or false)
+    // bool student = false;
+    // bool power = true;
+
+    // string (objects that represents a sequence of text)
+    // std::string name = "Ayliozaa";
+    // std::string day = "Friday";
+
+    // Output
+    // std::cout << "Hello " << name << '\n';
+    // std::cout << "You Are " << age << " years old" << '\n';
+    // std::cout << "Your Temperature Is " << temperature << " gradusi" << '\n';
+    // std::cout << "Your Balance Is " << price << '\n';
+
+    // the const keyboard specfies that a variable's value is constant
+    // tells the compiler to prevent from modifying it
+    // (Read-only)
+    // double PI = 3.14159;
+    // PI = 420.69;
+    // double radius = 10;
+    // double circumference = 2 * PI * radius;
+    // std::cout << circumference << "cm" << '\n';
+
+    // namespace = provides a solution for preventing name conflicts in large projects. Each entity needs a Unique name.
+    //             A namespace allows for Identically named entities as long as the namespaces are different.
+    // using namespace second;
+    // std::cout << first::x << " ara";
+    // std::cout << x << " ara" << '\n';
+
+    // typedef = reserved ketword used to create an additional name (alias) for another data type.
+    //           New identifier for an existing type helps with readability and reduces types
+    //           Use when there is a clear benefit replacedwith 'using' (work better w/ templates)
+    // pairlist_t pairlist;
+    // text_t firstName = "Reapera";
+    // number_t Aage = 18;
+    // std::cout << "Name: " << firstName << '\n' << "Age: " << Aage << '\n';
+
+    // arithmetic operators = return the result of a specific arithmetic operation (+ - * /)
+    // int students = 20;
+    // students = students + 1;
+    // students+=1;
+    // students = students -1;
+    // students-= 1;
+    // students = students * 2;
+    // students *=2;
+    // students = students / 2;
+    // students/= 2;
+    // int students = 6-(5+4)*3/2;
+    // std::cout << students << '\n';
+
+
+
+
+
+    return 0;
+}
